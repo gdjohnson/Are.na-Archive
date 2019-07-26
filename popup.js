9800e203013425b0f1dcf248a298b3b53ee0777c
Graham Johnson <suspendedreason@gmail.com>
2019-07-26 16:14:51 -0400

    Switching over to dedicated page

--- popup.js
+++ popup.js
@@ -1,25 +1,86 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var archiveButton = document.getElementById('archivePage');
-    archiveButton.addEventListener('click', archivePage)
-});
+const Arena = require('are.na');
+const fetch = require('node-fetch');
 
-var archiveLink;
 
-function archivePage() {
-    // const url = window.location.href;
-    const url = "wikipedia.org"
-    fetch(`http://archive.org/wayback/available?url=${url}`).then(data => {
-        if (!data.archived_snapshots.closest) { 
-            savePage(data, url)
-        } else {
-            archiveLink = data.archived_snapshots.closest.url;
+var allChans = [];
+var precarious = [];
+var preserved = [];
+
+function archiveLinks(accessToken){
+    arena = new Arena({accessToken});
+    
+    let id=13854;
+    fetchChans(id)
+}
+
+function fetchChans(id){
+    arena.user(id).channels()
+        .then(chans => {
+            chans.map(chan => {
+                const {id, title, contents} = chan;
+                allChans.push({id, title, contents});
+            })
+        })
+        .then(() => findLinks(allChans[6]))
+        .then(linkBlocks => extractSources(linkBlocks))
+        .then(sources => checkArchive(sources))
+        .then(() => { console.log(precarious) })
+        .catch(err => { console.error(err)})
+}
+
+function findLinks(chan){
+    return chan.contents.map(block => {
+        if (block.class = 'Link') { return block; }
+    })
+}
+
+function extractSources(linkBlocks) {
+    let sources = linkBlocks.map(block => {
+        if (block.source) {
+            return block.source.url;
         }
-    }).catch(err => console.error(err))
+    })
 
+    return sources.filter( Boolean )
 }
 
-function savePage(data, url) {
-    fetch(`https://web.archive.org/save/${url}`).then(
-                fetch(`http://archive.org/wayback/available?url=${url}`)).then(
-                    () => { archiveLink = data.archived_snapshots.closest.url;})
+
+function checkArchive(sources) {
+    sources.forEach(source => {
+        singleCheck(source);
+    })
 }
+
+function singleCheck(source) {
+    source = source.replace(/(^\w+:|^)\/\//, '');
+    console.log("The source is " + source)
+    fetch(`http://archive.org/wayback/available?url=${source}`)
+        .then(data => data.json())
+        .then(json => {
+            console.log("THE JSON IS " + json)
+            if (!json.archived_snapshots) { 
+                // console.log(source + " is precarious.")
+                precarious.push(source);
+            } else {
+                // console.log(json.archived_snapshots.closest.url + " is preserved");
+                preserved.push(json.archived_snapshots.closest.url);
+            }
+        })
+        .catch(err => console.error(err))
+    }
+
+// function savePage(data, url) {
+//     fetch(`https://web.archive.org/save/${url}`).then(
+//                 fetch(`http://archive.org/wayback/available?url=${url}`)).then(
+//                     () => { archiveLink = data.archived_snapshots.closest.url;})
+// }
+
+
+
+archiveLinks('')
+
+// data = {"url": "https://burnaway.org/feature/theory-in-studio-john-dewey-champion-of-art-as-experience/", "archived_snapshots": {"closest": {"status": "200", "available": true, "url": "http://web.archive.org/web/20171108134414/http://burnaway.org/feature/theory-in-studio-john-dewey-champion-of-art-as-experience/", "timestamp": "20171108134414"}}}
+// data = JSON.stringify(data)
+// console.log(data)
+// console.log(JSON.parse(data))
+

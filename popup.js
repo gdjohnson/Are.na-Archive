16db81a3c9bb78da691ded95d0d9019182d1f255
Graham Johnson <suspendedreason@gmail.com>
2019-07-26 20:04:04 -0400

    Switching to Await/Async. Still need to implement Try/Catch for res.json()

--- popup.js
+++ popup.js
@@ -6,26 +6,25 @@ var allChans = [];
 var precarious = [];
 var preserved = [];
 
-function archiveLinks(accessToken){
+async function archiveLinks(accessToken){
     arena = new Arena({accessToken});
     
     let id=13854;
-    fetchChans(id)
+    let results = await fetchChans(id);
+    return results;
 }
 
-function fetchChans(id){
-    arena.user(id).channels()
-        .then(chans => {
-            chans.map(chan => {
+async function fetchChans(id){
+    let chans = await arena.user(id).channels()
+    chans.map(chan => {
                 const {id, title, contents} = chan;
                 allChans.push({id, title, contents});
             })
-        })
-        .then(() => findLinks(allChans[6]))
-        .then(linkBlocks => extractSources(linkBlocks))
-        .then(sources => checkArchive(sources))
-        .then(() => { console.log(precarious) })
-        .catch(err => { console.error(err)})
+    let linkBlocks = findLinks(allChans[4])
+    let sources = extractSources(linkBlocks)
+    let classification = await checkArchive(sources))
+
+    return classification;
 }
 
 function findLinks(chan){
@@ -44,43 +43,44 @@ function extractSources(linkBlocks) {
     return sources.filter( Boolean )
 }
 
-
-function checkArchive(sources) {
-    sources.forEach(source => {
+async function checkArchive(sources) {
+    let results = await sources.map(source => {
         singleCheck(source);
     })
+
+    return results
 }
 
-function singleCheck(source) {
-    source = source.replace(/(^\w+:|^)\/\//, '');
-    console.log("The source is " + source)
-    fetch(`http://archive.org/wayback/available?url=${source}`)
-        .then(data => data.json())
-        .then(json => {
-            console.log("THE JSON IS " + json)
-            if (!json.archived_snapshots) { 
-                // console.log(source + " is precarious.")
-                precarious.push(source);
-            } else {
-                // console.log(json.archived_snapshots.closest.url + " is preserved");
-                preserved.push(json.archived_snapshots.closest.url);
-            }
+// [{preserved:true, arxLink:'', originalUrl:''}]
+
+async function singleCheck(url) {
+    let obj = {};
+    let res = await fetch(`http://archive.org/wayback/available?url=${url}*&output=json`);
+    res.json()
+        .then((res) => {
+            if (res["archived_snapshots"]) { 
+                obj[preserved] = true;
+                obj[arxLink] = res.archived_snapshots.closest.url;
+            } 
+        }, () => { 
+            savePage(url);
         })
-        .catch(err => console.error(err))
-    }
+        .then(()=>{
+            console.log(`Missing links include: `); 
+            console.log(precarious);
+            console.log(`Preserved links include: `);
+            console.log(preserved);
+        })
+        .then(())
+}
 
-// function savePage(data, url) {
-//     fetch(`https://web.archive.org/save/${url}`).then(
-//                 fetch(`http://archive.org/wayback/available?url=${url}`)).then(
-//                     () => { archiveLink = data.archived_snapshots.closest.url;})
-// }
+function savePage(url) {
+    console.log("SAVING PAGE NOW")
+    return fetch(`https://web.archive.org/save/${url}`)
+        .then(response => response.json())
+        .catch(() => console.log(`The page at ${url} can't be archived.`))
+        .then(() => { precarious.push(url) })
+}
 
 
-
-archiveLinks('')
-
-// data = {"url": "https://burnaway.org/feature/theory-in-studio-john-dewey-champion-of-art-as-experience/", "archived_snapshots": {"closest": {"status": "200", "available": true, "url": "http://web.archive.org/web/20171108134414/http://burnaway.org/feature/theory-in-studio-john-dewey-champion-of-art-as-experience/", "timestamp": "20171108134414"}}}
-// data = JSON.stringify(data)
-// console.log(data)
-// console.log(JSON.parse(data))
-
+archiveLinks('').then(console.log("PRESERVED FINAL " + preserved))

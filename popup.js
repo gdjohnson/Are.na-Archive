9391d11e9b753efaa97590fa6c08d9a02e6909d9
Graham Johnson <suspendedreason@gmail.com>
2019-07-27 17:20:09 -0400

    Switching to await/async, checking for pre-existing Wayback links

--- popup.js
+++ popup.js
@@ -3,27 +3,32 @@ const fetch = require('node-fetch');
 
 
 var allChans = [];
-var precarious = [];
-var preserved = [];
 
-async function archiveLinks(accessToken){
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.getElementById('archivePage').addEventListener('click', archiveLinks)
+// })
+
+function archiveLinks(){
+    // const accessToken = document.forms['archiveNow']['code'].value
+    const accessToken = '';
     arena = new Arena({accessToken});
     
     let id=13854;
-    let results = await fetchChans(id);
-    return results;
+    fetchChans(id)
 }
 
 async function fetchChans(id){
     let chans = await arena.user(id).channels()
-    chans.map(chan => {
+    chans = chans.map(chan => {
                 const {id, title, contents} = chan;
-                allChans.push({id, title, contents});
+                return ({id, title, contents});
             })
-    let linkBlocks = findLinks(allChans[4])
-    let sources = extractSources(linkBlocks)
-    let classification = await checkArchive(sources))
-
+    let linkBlocks = chans.map(chan => findLinks(chan)).filter(arr => arr.length > 0);
+    debugger
+    let sources = linkBlocks.map(chan => extractSources(chan)).filter(arr => arr.length > 0);
+    debugger
+    let classification = await sources.map(chan => checkArchive(chan));
+    console.log(classification)
     return classification;
 }
 
@@ -33,21 +38,23 @@ function findLinks(chan){
     })
 }
 
-function extractSources(linkBlocks) {
-    let sources = linkBlocks.map(block => {
+function extractSources(chan) {
+    let sources = chan.map(block => {
         if (block.source) {
             return block.source.url;
         }
     })
 
-    return sources.filter( Boolean )
+    return sources.filter(Boolean)        
 }
 
-async function checkArchive(sources) {
-    let results = await sources.map(source => {
-        singleCheck(source);
+async function checkArchive(chan) {
+    debugger
+    let results = await chan.map(async (source) => {
+        await singleCheck(source);
     })
-
+    debugger
+    // RIGHT NOW ALL RESULTS ARE RETURNING UNDEFINED
     return results
 }
 
@@ -55,32 +62,63 @@ async function checkArchive(sources) {
 
 async function singleCheck(url) {
     let obj = {};
+    if (filterWaybackLinks(url)) { return ({preserved: true, arxLink: url})}
     let res = await fetch(`http://archive.org/wayback/available?url=${url}*&output=json`);
-    res.json()
-        .then((res) => {
-            if (res["archived_snapshots"]) { 
-                obj[preserved] = true;
-                obj[arxLink] = res.archived_snapshots.closest.url;
-            } 
-        }, () => { 
-            savePage(url);
-        })
-        .then(()=>{
-            console.log(`Missing links include: `); 
-            console.log(precarious);
-            console.log(`Preserved links include: `);
-            console.log(preserved);
-        })
-        .then(())
+    debugger
+    try { 
+        res = await res.json() 
+        if (res["archived_snapshots"]) { 
+            obj.preserved = true;
+            obj.arxLink = res.archived_snapshots.closest.url;
+            obj.liveLink = url;
+        } 
+        else { 
+            console.error("LINE 75") 
+        }
+    }
+    // ALL THE OBJS ARE FAILING
+    catch { obj = await savePage(url) }
+        
+    // appendToPage(obj);
+    // console.log(obj)
+    debugger
+    return obj;
 }
 
-function savePage(url) {
-    console.log("SAVING PAGE NOW")
-    return fetch(`https://web.archive.org/save/${url}`)
-        .then(response => response.json())
-        .catch(() => console.log(`The page at ${url} can't be archived.`))
-        .then(() => { precarious.push(url) })
+function filterWaybackLinks(str) {
+    if (str.includes('web.archive.org')) {
+		return true;
+    }
+    return false;
+} 
+
+async function savePage(url) {
+    let obj = {};
+    
+    let res;
+    try {
+        // console.log(`Saving ${url} now.`)
+        res = await fetch(`https://web.archive.org/save/${url}`);
+        // console.log(`LINE 92 Results: ` + res)
+        // NEEDS TO CREATE OBJ FOR SUCCESSES
+    }
+    catch {
+        // console.log(`The page at ${url} can't be archived.`)
+        obj.preserved = false;
+        obj.arxLink = '';
+        obj.liveLink = url;
+        // console.log(obj)
+    }
+    
+    return obj;
 }
 
+function appendToPage(obj) {
+    let graf = document.createElement('p');
+    graf.innerText =    `Preserved: ${obj.preserved}   
+                         Archival link: ${obj.arxLink}   
+                         Live link: ${obj.liveLink}`
+    document.querySelector('archiveNow').append(graf);
+}
 
-archiveLinks('').then(console.log("PRESERVED FINAL " + preserved))
+archiveLinks()
